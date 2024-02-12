181059d9cbc9befabb9a1c2ae49fea0d4b4a571f
shipranshi keshri <keshrishipranshi@gmail.com>
2024-02-12 09:44:48 +0530

    commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 444;
+//not allowed
+//accountId = 4;
+console.log(accountId);
+
+var account = 234;
+account = 9;
+console.log(account);
+
+let name ;
+
+console.table([account, accountId,name]);
+
